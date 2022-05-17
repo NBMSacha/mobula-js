4aaf11ba2745fbd0aa03d54e7db0eed4925cd3ee
Sacha Marcus <89188530+NBMSacha@users.noreply.github.com>
2022-05-17 15:48:54 +0200

    Updating to Protocol V2

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const ethers = require('ethers');
 const axios = require('axios');
 
 module.exports = class Mobula {
-    async getData(tokenAddress) {
+    async getMetaData(tokenAddress) {
         const provider = ethers.getDefaultProvider('https://polygon-rpc.com');
-        const API_ADDRESS = '0x76edF9562F2Cca3bc36DB2ed58A4adC0b10F1048'
+        const API_ADDRESS = '0x4f93eed1fC945b5bC3FaAEc5C057DFf27dB31DBA'
         const apiContract = new ethers.Contract(API_ADDRESS,
             ['function staticData(address token) external view returns(string)'], provider)
 
         const dataHash = await apiContract.staticData(tokenAddress)
-        const r = await axios.get('https://gateway.ipfs.io/ipfs/' + dataHash)
-        return r.data
+
+        try {
+            const r = await axios.get('https://gateway.ipfs.io/ipfs/' + dataHash)
+            return r.data
+        } catch (err) {
+            return {}
+        }
     }
 }
